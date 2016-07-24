1740e1ceac06733498a33211428c9125f04f2c96
hypatiah <danielle.hassid@gmail.com>
2016-07-24 14:20:44 -0700

    Fix drive routes to create new drive

--- app/controllers/drives-controller.rb
+++ app/controllers/drives-controller.rb
@@ -2,16 +2,17 @@ get '/drives' do
     erb :"drives/index"
 end
 
-get 'drives/new' do
-    erb :"drives/new"
+get '/drives/new' do
+    erb :'drives/new'
 end
 
-post 'drives/new' do
-    redirect 'drives/new'
+post '/drives/new' do
+    @drive = Drive.create(user_id: session[:user_id], start_address: 4.0, end_address: 2.0, departure_min: params[:time_start], departure_max: params[:time_end], seats_available: params[:passengers], tolerance_in_miles: 3)
+    redirect "/drives/#{@drive.id}/matches"
 end
 
 post '/drives' do
-    redirect 'drives/index'
+    redirect '/drives/index'
 end
 
 get '/drives/:id/matches' do
