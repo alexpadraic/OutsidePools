7e744e4ea4f02d1e8ca441dbf66d13afb56c2af2
raj7desai <raj7desai@gmail.com>
2016-07-24 05:31:54 -0700

    Add final files for rider/driver views and add

--- HTMLCSS/js/application.js
+++ HTMLCSS/js/application.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+
+  $('#contact-driver-button a').on('click', function(event){
+    event.preventDefault();
+    // debugger;
+    var s= this.parentElement.parentElement.children[5];
+    $(s).toggleClass('hidden');
+  });
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
